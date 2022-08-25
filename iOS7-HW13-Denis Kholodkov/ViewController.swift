1ad8d2bdf00bf0d17e40e0a57550ed96c3099447
Denis <den88000@gmail.com>
2022-08-25 17:36:42 +0300

    Initial Commit

---
+++ iOS7-HW13-Denis Kholodkov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS7-HW13-Denis Kholodkov
+//
+//  Created by Денис Холодков on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
